1d5fdf3079c3ee9ac844475cd33d1c6140f151c0
Hugo De Moraes <demoraeshugo@gmail.com>
2022-06-15 03:30:13 -0400

    exposing error classes to api

--- src/main.js
+++ src/main.js
@@ -4,4 +4,18 @@ import Speaker from './peer-connection/speaker';
 import VolumeCalibration from './tasks/volume/volume';
 import ImpulseResponseCalibration from './tasks/impulse-response/impulseResponse';
 
-export {Listener, Speaker, VolumeCalibration, ImpulseResponseCalibration};
+import {
+  UnsupportedDeviceError,
+  MissingSpeakerIdError,
+  CalibrationTimedOutError,
+} from './peer-connection/peerErrors';
+
+export {
+  Listener,
+  Speaker,
+  VolumeCalibration,
+  ImpulseResponseCalibration,
+  UnsupportedDeviceError,
+  MissingSpeakerIdError,
+  CalibrationTimedOutError,
+};
