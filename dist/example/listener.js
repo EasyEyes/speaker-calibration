2181c8184277d3a7f82527a7ac93678ca74683b8
Nathnael Hailu Tsegaye <nht249@nyu.edu>
2023-07-17 18:31:39 +0400

    cosmetic changes for listener page

--- dist/example/listener.js
+++ dist/example/listener.js
@@ -1,59 +1,61 @@
 // get element with id message
 import {phrases} from './i18n.js';
-const message = document.getElementById('message');
-message.style.lineHeight = '2.5rem';
+
 // get url query parameters
 const urlParams = new URLSearchParams(window.location.search);
 
 // get isSmartPhone query parameter
 const isSmartPhone = urlParams.get('isSmartPhone');
 
-switch (isSmartPhone) {
-  case 'true':
-    message.innerHTML = phrases.RC_phoneMicrophoneInstructions['en-US'].replace(/\n/g, '<br>');
-    break;
-  case 'false':
-    message.innerHTML = phrases.RC_usbMicrophoneInstructions['en-US'].replace(/\n/g, '<br>');
-}
-
 const listenerParameters = {
   targetElementId: 'display',
 };
-document.getElementById('calibrationBeginButton').onclick = () => {
+
+const recordingInProgress = phrases.RC_soundRecording['en-US'];
+const backToExperimentWindow = phrases.RC_backToExperimentWindow['en-US'];
+const allowMicrophone = phrases.RC_allowMicrophoneUse['en-US'];
+const recordingInProgressElement = document.getElementById('recordingInProgress');
+const allowMicrophoneElement = document.getElementById('allowMicrophone');
+
+// set the text of the html elements
+recordingInProgressElement.innerHTML = recordingInProgress;
+allowMicrophoneElement.innerHTML = allowMicrophone;
+
+recordingInProgressElement.style.whiteSpace = 'nowrap';
+recordingInProgressElement.style.fontWeight = 'bold';
+// fit content
+recordingInProgressElement.style.width = 'fit-content';
+let fontSize = 100;
+recordingInProgressElement.style.fontSize = fontSize + 'px';
+
+while (recordingInProgressElement.scrollWidth > window.innerWidth * 0.9 && fontSize > 10) {
+  fontSize--;
+  recordingInProgressElement.style.fontSize = fontSize + 'px';
+}
+
+switch (isSmartPhone) {
+  case 'true':
+    // show the html upsidedown
+    const phrasesContainer = document.getElementById('phrases');
+    // add class
+    phrasesContainer.classList.add('phrases');
+    const html = document.querySelector('html');
+    html.style.overflow = 'hidden';
+    const display = document.getElementById('updateDisplay');
+    display.classList.add('updateDisplay');
+
+    break;
+  case 'false':
+    const message = document.getElementById('message');
+    message.style.lineHeight = '2.5rem';
+    const p = document.createElement('p');
+    p.innerHTML = backToExperimentWindow;
+    message.appendChild(p);
+    break;
+}
+
+// event listener to start calibration when the page is loaded
+window.addEventListener('load', () => {
   window.listener = new speakerCalibrator.Listener(listenerParameters);
   console.log(window.listener);
-  document.querySelector('#calibrationBeginButton').style.display = 'none';
-
-  const recordingInProgress = phrases.RC_soundRecording['en-US'];
-  const backToExperimentWindow = phrases.RC_backToExperimentWindow['en-US'];
-  document.querySelector('#message').innerHTML = recordingInProgress;
-  document.querySelector('#message').style.fontSize = '2em';
-
-  console.log('isSmartPhone', isSmartPhone);
-  console.log('isNotSmartPhone', !isSmartPhone);
-  switch (isSmartPhone) {
-    case 'false':
-      const p = document.createElement('p');
-      p.innerHTML = backToExperimentWindow;
-      p.style.fontSize = '0.7em';
-      document.querySelector('#message').appendChild(p);
-      break;
-    case 'true':
-      break;
-  }
-};
-// double the size of #message
-
-// enter key press
-document.addEventListener('keydown', event => {
-  if (event.key === 'Enter') {
-    window.listener = new speakerCalibrator.Listener(listenerParameters);
-    console.log(window.listener);
-    document.querySelector('#calibrationBeginButton').style.display = 'none';
-    document.querySelector('#message').innerHTML =
-      phrases.RC_soundRecording['en-US'] + '\n' + isSmartPhone
-        ? ''
-        : phrases.RC_backToExperimentWindow['en-US'];
-    document.querySelector('#message').style.fontSize = '2em';
-  }
 });
