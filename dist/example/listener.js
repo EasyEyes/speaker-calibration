8f8a21172ad9a82f81582e0648ac0b4accf47169
Nati Tsegaye <nht249@nyu.edu>
2023-07-24 17:42:43 +0400

    cosmetic changes for listener page

--- dist/example/listener.js
+++ dist/example/listener.js
@@ -14,48 +14,73 @@ const listenerParameters = {
 const recordingInProgress = phrases.RC_soundRecording['en-US'];
 const backToExperimentWindow = phrases.RC_backToExperimentWindow['en-US'];
 const allowMicrophone = phrases.RC_allowMicrophoneUse['en-US'];
+const placeSmartphoneMicrophone = phrases.RC_placeSmartphoneMicrophone['en-US'];
 const recordingInProgressElement = document.getElementById('recordingInProgress');
 const allowMicrophoneElement = document.getElementById('allowMicrophone');
 
-// set the text of the html elements
-recordingInProgressElement.innerHTML = recordingInProgress;
-allowMicrophoneElement.innerHTML = allowMicrophone;
-
-recordingInProgressElement.style.whiteSpace = 'nowrap';
-recordingInProgressElement.style.fontWeight = 'bold';
-// fit content
-recordingInProgressElement.style.width = 'fit-content';
-let fontSize = 100;
-recordingInProgressElement.style.fontSize = fontSize + 'px';
-
-while (recordingInProgressElement.scrollWidth > window.innerWidth * 0.9 && fontSize > 10) {
-  fontSize--;
-  recordingInProgressElement.style.fontSize = fontSize + 'px';
-}
-
 switch (isSmartPhone) {
   case 'true':
-    // show the html upsidedown
-    const phrasesContainer = document.getElementById('phrases');
-    // add class
-    phrasesContainer.classList.add('phrases');
-    const html = document.querySelector('html');
-    html.style.overflow = 'hidden';
-    const display = document.getElementById('updateDisplay');
-    display.classList.add('updateDisplay');
+    allowMicrophoneElement.innerHTML = placeSmartphoneMicrophone;
+    // event listener for id calibrationBeginButton
+    const calibrationBeginButton = document.getElementById('calibrationBeginButton');
+    calibrationBeginButton.addEventListener('click', () => {
+      // remove the button
+      calibrationBeginButton.remove();
+      // set the text of the html elements
+      recordingInProgressElement.innerHTML = recordingInProgress;
+      allowMicrophoneElement.innerHTML = allowMicrophone;
 
+      recordingInProgressElement.style.whiteSpace = 'nowrap';
+      recordingInProgressElement.style.fontWeight = 'bold';
+      // fit content
+      recordingInProgressElement.style.width = 'fit-content';
+      let fontSize = 100;
+      recordingInProgressElement.style.fontSize = fontSize + 'px';
+
+      while (recordingInProgressElement.scrollWidth > window.innerWidth * 0.9 && fontSize > 10) {
+        fontSize--;
+        recordingInProgressElement.style.fontSize = fontSize + 'px';
+      }
+      // show the html upsidedown
+      const phrasesContainer = document.getElementById('phrases');
+      // add class
+      phrasesContainer.classList.add('phrases');
+      const html = document.querySelector('html');
+      html.style.overflow = 'hidden';
+      const display = document.getElementById('updateDisplay');
+      display.classList.add('updateDisplay');
+      window.listener = new speakerCalibrator.Listener(listenerParameters);
+      console.log(window.listener);
+    });
     break;
   case 'false':
-    const message = document.getElementById('message');
-    message.style.lineHeight = '2.5rem';
-    const p = document.createElement('p');
-    p.innerHTML = backToExperimentWindow;
-    message.appendChild(p);
+    // remove the button
+    const calibrationBeginButton2 = document.getElementById('calibrationBeginButton');
+    calibrationBeginButton2.remove();
+    // event listener for when the page is loaded
+    window.addEventListener('load', () => {
+      // set the text of the html elements
+      recordingInProgressElement.innerHTML = recordingInProgress;
+      allowMicrophoneElement.innerHTML = allowMicrophone;
+
+      recordingInProgressElement.style.whiteSpace = 'nowrap';
+      recordingInProgressElement.style.fontWeight = 'bold';
+      // fit content
+      recordingInProgressElement.style.width = 'fit-content';
+      let fontSize = 100;
+      recordingInProgressElement.style.fontSize = fontSize + 'px';
+
+      while (recordingInProgressElement.scrollWidth > window.innerWidth * 0.9 && fontSize > 10) {
+        fontSize--;
+        recordingInProgressElement.style.fontSize = fontSize + 'px';
+      }
+      const message = document.getElementById('message');
+      message.style.lineHeight = '2.5rem';
+      const p = document.createElement('p');
+      p.innerHTML = backToExperimentWindow;
+      message.appendChild(p);
+      window.listener = new speakerCalibrator.Listener(listenerParameters);
+      console.log(window.listener);
+    });
     break;
 }
-
-// event listener to start calibration when the page is loaded
-window.addEventListener('load', () => {
-  window.listener = new speakerCalibrator.Listener(listenerParameters);
-  console.log(window.listener);
-});
