13d35495ce43d61fa907f5c0b4cb2575c1694619
Nati Tsegaye <nht249@nyu.edu>
2023-08-03 20:35:34 +0400

    stop orientation lock for debugging

--- dist/example/listener.js
+++ dist/example/listener.js
@@ -22,7 +22,7 @@ const turnMessageElement = document.getElementById('turnMeToReadBelow');
 
 switch (isSmartPhone) {
   case 'true':
-    window.screen.orientation.lock('portrait');
+    // window.screen.orientation.lock('portrait');
     allowMicrophoneElement.innerHTML = placeSmartphoneMicrophone;
     turnMessageElement.innerHTML = turnMeToReadBelow;
     // show the html upsidedown
