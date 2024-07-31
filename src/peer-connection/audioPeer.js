d94cdd03e2601200b3aafd01ba38909175a3ee8e
Nati Tsegaye <nht249@nyu.edu>
2024-08-01 02:37:38 +0400

    add turn server to peer

--- src/peer-connection/audioPeer.js
+++ src/peer-connection/audioPeer.js
@@ -36,6 +36,33 @@ class AudioPeer {
       secure: true,
       host: 'easyeyes-peer-server.herokuapp.com',
       port: 443,
+      config: {
+        iceServers: [
+          {
+            urls: 'stun:stun.relay.metered.ca:80',
+          },
+          {
+            urls: 'turn:global.relay.metered.ca:80',
+            username: 'de884cfc34189cdf1a5dd616',
+            credential: 'IcOpouU9/TYBmpHU',
+          },
+          {
+            urls: 'turn:global.relay.metered.ca:80?transport=tcp',
+            username: 'de884cfc34189cdf1a5dd616',
+            credential: 'IcOpouU9/TYBmpHU',
+          },
+          {
+            urls: 'turn:global.relay.metered.ca:443',
+            username: 'de884cfc34189cdf1a5dd616',
+            credential: 'IcOpouU9/TYBmpHU',
+          },
+          {
+            urls: 'turns:global.relay.metered.ca:443?transport=tcp',
+            username: 'de884cfc34189cdf1a5dd616',
+            credential: 'IcOpouU9/TYBmpHU',
+          },
+        ],
+      },
     });
     this.conn = null;
     this.lastPeerId = null;
